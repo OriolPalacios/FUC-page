fd85a84cddf018c019660ba4dc177d2874cd27e5
Correo institucional - Oriol <211356@unsaac.edu.pe>
2025-01-21 18:55:06 -0500

    feat: institution quote added below hero

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      color: {
+        institution: 'var(--institution-color)',
+      },
       brightness: {
         60: '.6',
       }
