baf1638913dd7127d9fa0f3464ee2b6ad60844fb
Oriol Palacios <orioloriolpalacios@outlook.com>
2025-02-04 11:39:27 -0500

    chore&feat: migration from using google sheet db to nocodb, removing older services

--- src/services/news.ts
+++ src/services/news.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import api from "../utils/api";
 
 export async function getNews() {
@@ -6,5 +7,19 @@ export async function getNews() {
     return response.data;
   } catch (error) {
     console.error(error);
+  }
+}
+
+export async function getNewsPerId(id: string) {
+  try {
+    const response = await api.get(import.meta.env.NOTICIAS_V1_DEFAULT_VIEW + '/records/' + id)
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response?.status === 400 ) {
+      throw new Error('Noticia no encontrada');
+    } else {
+      console.error(error);
+      throw new Error('Error al obtener la noticia');
+    }
   }
 }
