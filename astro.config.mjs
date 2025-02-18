eda2cb12311140de71f7a17d0c0c6bde7fcd5fe3
Oriol Palacios <orioloriolpalacios@outlook.com>
2025-02-18 17:22:14 -0500

    fix: correct internal redirection

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
     '/news': '/news/page/1',
     '/home': '/',
     '/news/admin' : 'https://app.nocodb.com/#/nc/form/d6dc6d04-54a0-401e-a474-7228951f81f1',
-    'documents': 'documents/page/1',
+    'documents': '/documents/page/1',
   },
 
   adapter: netlify()
