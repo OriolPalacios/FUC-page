b8bc98a399c9857939fbd674122d6a63980e30b4
Oriol Palacios <orioloriolpalacios@outlook.com>
2025-03-10 10:00:14 -0500

    fix(cache): removing cache boosting headers on request

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,15 @@
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware(async ({ request, locals, url }, next) => {
+  const newRequest = new Request(request.url, {
+    method: request.method,
+    headers: new Headers(request.headers),
+    body: request.body,
+    redirect: request.redirect,
+    signal: request.signal
+  });
+  newRequest.headers.delete('cache-control');
+  newRequest.headers.delete('pragma');
   const response = await next();
   if (url.pathname.includes('_server-islands')) {
     const referer = request.headers.get('Referer');
@@ -10,6 +19,7 @@ export const onRequest = defineMiddleware(async ({ request, locals, url }, next)
       response.headers.set('Cache-Control', 'public, max-age=0');
       response.headers.set('Netlify-CDN-Cache-Control', 'public, durable, s-maxage=300, stale-while-revalidate=3500');
       response.headers.set('Netlify-vary', `path=${url.pathname}&page=${pageParam}`);
+      response.headers.set('X-Content-Generated', new Date().toISOString());
     }
   }
   return response;
