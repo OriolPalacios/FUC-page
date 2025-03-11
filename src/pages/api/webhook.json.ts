a8a4d5ae614e8108e15d337955ec462bc33fcfd0
Oriol Palacios <orioloriolpalacios@outlook.com>
2025-03-11 11:06:41 -0500

    feat(cache): invalidate cache via tags on cdn via webhooks

---
+++ src/pages/api/webhook.json.ts
@@ -0,0 +1,39 @@
+import { purgeCache } from "@netlify/functions";
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ request }) => {
+  if (request.headers.get("X-Contentful-Webhook-Secret") !== import.meta.env.CONTENTFUL_WEBHOOK_SECRET) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+  const body = await request.json();
+  console.log(body.data.rows[0].Id);
+  let section = body.data.table_name.split('-')[0];
+  switch (section) {
+    case 'noticias':
+      section = 'news';
+      break;
+    case 'miembros':
+      section = 'members';
+      break;
+    case 'eventos':
+      section = 'events';
+      break;
+    case 'documentos':
+      section = 'documents';
+      break;
+    default:
+      return new Response(`Incorrect section name`, {status: 400})
+  }
+  const id = body.data.rows[0].Id;
+  const test_response = `Revalidated entry with for section ${section} and for id ${id}`;
+  try {
+    await purgeCache({ tags: [`${section}`, `${section}-page-detail-${id}`] });
+    return new Response(`Revalidated entry for section ${section} and detail page ${section}-page-detail-${id}`, { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({
+      message: "This was a POST!",
+      body: body,
+      test_response: test_response
+    }))
+  }
+}
